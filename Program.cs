9703a1c56cc303355f758a9426245fb11bb12217
AlexanderChunarev <alexanderchunarov1999@gmial.com>
2020-07-27 15:01:24 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InCamp_CsvProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
