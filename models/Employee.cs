22414e0fb03111e3192bd82544040a2cd547ae98
AlexanderChunarev <alexanderchunarov1999@gmial.com>
2020-07-27 15:08:21 +0300

    created models

---
+++ models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace InCamp_CsvProcessor.models
+{
+    class Employee
+    {
+        public string EmployeeName { get; set; }
+        public DateTime Date { get; set; }
+        public float Hours { get; set; }
+    }
+}
---
+++ models/EmployeeMap.cs
@@ -0,0 +1,12 @@
+﻿namespace InCamp_CsvProcessor.models
+{
+    class EmployeeMap : CsvHelper.Configuration.ClassMap<Employee>
+    {
+        public EmployeeMap()
+        {
+            Map(m => m.EmployeeName).Name("Employee Name");
+            Map(m => m.Date).Name("Date");
+            Map(m => m.Hours).Name("Work Hours");
+        }
+    }
+}
